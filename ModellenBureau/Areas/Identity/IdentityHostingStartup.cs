e3a967f619b38ffb2781c55a5365cdd5a4ee2203
pieterbrandsen <45623682+pieterbrandsen@users.noreply.github.com>
2020-08-18 11:37:21 +0200

    push

--- ModellenBureau/Areas/Identity/IdentityHostingStartup.cs
+++ ModellenBureau/Areas/Identity/IdentityHostingStartup.cs
@@ -15,6 +15,18 @@ namespace ModellenBureau.Areas.Identity
         public void Configure(IWebHostBuilder builder)
         {
             builder.ConfigureServices((context, services) => {
+                //services.AddDbContext<ApplicationDbContext>(options =>
+                //    options.UseSqlServer(
+                //        context.Configuration.GetConnectionString("AuthDbContextConnection")));
+
+
+                //services.AddDefaultIdentity<ApplicationUser>(options =>
+                //{
+                //    options.SignIn.RequireConfirmedAccount = false;
+                //    options.Password.RequireLowercase = false;
+                //    options.Password.RequireUppercase = false;
+                //})
+                //.AddEntityFrameworkStores<ApplicationDbContext>();
             });
         }
     }
